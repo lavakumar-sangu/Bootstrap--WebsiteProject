ca2004ca0d30ee32041d69d5ea11a6980b4b8d52
Lava Kumar Sangu <lsangu@keypoint-tech.com>
2021-08-16 13:07:27 +0530

    moved stlyes, commented the code

--- img/script.js
+++ img/script.js
@@ -1,4 +1,5 @@
-function openCity(evt, cityName) {
+// this Js code is necessary to move carousel-images automatically one by one in certain time, without clicking any button.
+function myCarousel(evt, carouselName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -8,6 +9,6 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(carouselName).style.display = "block";
     evt.currentTarget.className += " active";
 }
